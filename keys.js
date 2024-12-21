d7a159b8a6eb1738d188f6b4ca7ace9da51de1fd
Christian Braimah <chrisbraimah15@gmail.com>
2024-12-20 23:45:53 -0800

    API Ignore

---
+++ keys.js
@@ -0,0 +1,5 @@
+// Open Weather API Key
+export const api_key = "03f182ff8b10fb8ac04e4f828aa26bad";
+
+// Unsplash Access Key
+export const access_Key = "3qvsLH-bcKecve72SxUZXKXeOV46QmLRZd1pKjpyj64";
